6217cf7794049d1960dca3f07ef31c9a09fc7e01
JieNan <nan.j@northeastern.edu>
2025-05-02 18:04:41 -0700

    add ratelimit and circuit Breaker function

---
+++ version4/src/main/java/part1/Client/serviceCenter/ServiceCenter.java
@@ -0,0 +1,10 @@
+package part1.Client.serviceCenter;
+
+import java.net.InetSocketAddress;
+
+public interface ServiceCenter {
+    //  查询：根据服务名查找地址
+    InetSocketAddress serviceDiscovery(String serviceName);
+    //判断是否可重试
+    boolean checkRetry(String serviceName) ;
+}
